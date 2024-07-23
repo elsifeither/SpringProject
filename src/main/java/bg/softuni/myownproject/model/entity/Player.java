5339fe1470fafc9f100ddd5ed1f7e4c5711f483b
Ivo <Ivo@softuni>
2024-07-23 16:17:55 +0300

    add entities 2nd time

--- src/main/java/bg/softuni/myownproject/model/entity/Player.java
+++ src/main/java/bg/softuni/myownproject/model/entity/Player.java
@@ -3,6 +3,7 @@ package bg.softuni.myownproject.model.entity;
 
 import jakarta.persistence.*;
 
+import java.time.Instant;
 import java.time.LocalDate;
 
 @Entity
@@ -14,13 +15,70 @@ public class Player {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "first_name", nullable = false)
     private String firstName;
+
+    @Column(name = "last_name", nullable = false)
     private String lastName;
-    private LocalDate dateOfBirth;
+
+    @Column(name = "date_of_birth", nullable = false)
+    private Instant dateOfBirth;
+
+    @Column
     private String position;
 
     @ManyToOne
-    @JoinColumn(name = "team_id")
     private Team team;
 
+    public Player() {
+
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public Instant getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Instant dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
--- src/main/java/bg/softuni/myownproject/model/entity/Team.java
+++ src/main/java/bg/softuni/myownproject/model/entity/Team.java
@@ -2,6 +2,7 @@ package bg.softuni.myownproject.model.entity;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -12,13 +13,48 @@ public class Team {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String name;
 
-    @ManyToOne
-    @JoinColumn(name = "coach_id")
+    @ManyToOne()
     private Coach coach;
 
     @OneToMany(mappedBy = "team")
     private List<Player> players;
 
+    public Team() {
+        this.players = new ArrayList<>();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Coach getCoach() {
+        return coach;
+    }
+
+    public void setCoach(Coach coach) {
+        this.coach = coach;
+    }
+
+    public List<Player> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(List<Player> players) {
+        this.players = players;
+    }
 }
